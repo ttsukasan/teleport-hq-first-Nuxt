c03faf213100cdee8e70cf4a2af39d34e6da49fc
t.tsuka <30304956+ttsukasan@users.noreply.github.com>
2024-04-13 23:05:01 +0900

    Commit from TeleportHQ

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,4 @@
 export default {
   css: [`~/style.css`],
-  plugins: [`~/plugins/lottie-vue-player.client.js`],
-  router: {
-    base: '/teleport-hq-first-Nuxt/'
-  }
+  plugins: [`~/plugins/lottie-vue-player.client.js`]
 };
